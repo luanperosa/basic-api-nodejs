f962bf195303b86c79e15928f24ef85f699f63a0
luanperosa <luan.perosa@hotmail.com>
2019-08-25 15:51:41 -0300

    add async and await

--- aula1/routes/index.js
+++ aula1/routes/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  console.log('entrou no get')
+  console.log('entry of get raiz');
   return res.send({message: 'Get index raiz ok!'})
 })
 
 router.post('/', (req, res) => {
-  console.log('entrou no post')
+  console.log('entry of post raiz');
   return res.send({message: 'Post index raiz ok!'})
 })
 
--- aula1/routes/users.js
+++ aula1/routes/users.js
@@ -3,54 +3,89 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const Users = require('../models/users');
 
-router.get('/', (req, res) => {
-  Users.find({}, (err, data) => {
-    if(err) return res.send({error: 'Error select * from users'});
-    return res.send(data);
-  });
+router.get('/', async (req, res) => {
+  try {
+    const users = await Users.find({});
+    return res.send(users);
+  } catch (err) {
+    return res.send({ error: 'Error select * from users' });
+  }
+  
+  // Users.find({}, (err, data) => {
+  //   if(err) return res.send({error: 'Error select * from users'});
+  //   return res.send(data);
+  // });
   
   // return res.send({message: 'Get users raiz ok!'})
-})
+});
 
 router.post('/', (req, res) => {
-  return res.send({message: 'Post users raiz ok!'})
-})
+  return res.send({ message: 'Post users raiz ok!' });
+});
 
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
   const { email, password } = req.body;
+  if (!email || !password) return res.send({ error: 'Insuficient data' });
 
-  if(!email || !password) return res.send({ error: 'Error of dados' });
-  Users.findOne({email: email}, (err, data) => {
-    if(err) return res.send({ error: 'Error to select findOne Users'})
-    if(data) return res.send({ error: 'Users is Registered' });
+  try {
+    const userExist = await Users.findOne({ email });
+    if (userExist) return res.send({ error: 'Error, userExist is true' });
 
-    Users.create({email: email, password}, (err, data) => {
-      if(err) return res.send( { error: 'Erros to try create User' });
+    const newUser = await Users.create({ email, password });
+    newUser.password = undefined;
+    return res.send(newUser);
+  } catch (err) {
+    return res.send({ error: 'Users is Registered' });
+  }
+  
+  // if(!email || !password) return res.send({ error: 'Error of dados' });
+
+  // Users.findOne({email: email}, (err, data) => {
+  //   if(err) return res.send({ error: 'Error to select findOne Users'})
+  //   if(data) return res.send({ error: 'Users is Registered' });
+
+  //   Users.create({email: email, password}, (err, data) => {
+  //     if(err) return res.send( { error: 'Erros to try create User' });
       
-      data.password = undefined;
-      if(data) return res.send(data);
-    })
-  });
+  //     data.password = undefined;
+  //     if(data) return res.send(data);
+  //   })
+
+  // });
   
   //return res.send({message: 'Criado com sucesso'}) 
-})
+});
 
-router.post('/auth', (req, res) => {
+router.post('/auth', async (req, res) => {
   const { email, password } = req.body;
+  if (!email || !password) return res.send({ error: 'Insuficient data1' });
 
-  if (!email || !password) return res.render({ error: 'Users not exist!'});
+  try {
+    const userExist = await Users.findOne({ email }).select('+password'); //obrigation set the password of BD;  
+    if (!userExist) return res.send({ error: 'User is not exist!' });
 
-  Users.findOne({ email: email }, (err, data) => {
-    if(err) return res.send({ error: 'Erros to connect and select user'});
-    if(!data) return res.send({ error: 'User is not exist, no data!'});
-  // becareful not compare laranja with banana
-    bcrypt.compare(password, data.password, (err, same) => { 
-      if (!same) return res.send({ error: 'Error password invalid' });
+    const pass_ok = await bcrypt.compare(password, userExist.password);
+    if (!pass_ok) return res.send({ error: 'Error password invalid' });
 
-      data.password = undefined;
-      return res.send(data);
-    });
-  }).select('+password'); // obrigation set the password
+    userExist.password = undefined;
+    return res.send(userExist);
+    
+  } catch (err) {
+    if (err) return res.send({ error: 'Error to connect and select user' });
+  }
+  
+  // Users.findOne({ email: email }, (err, data) => {
+  //   if(err) return res.send({ error: 'Error to connect and select user'});
+  //   if(!data) return res.send({ error: 'User is not exist, no data!'});
+  // // becareful not compare laranja with banana
+  //   bcrypt.compare(password, data.password, (err, same) => { 
+  //     if (!same) return res.send({ error: 'Error password invalid' });
+
+  //     data.password = undefined;
+  //     return res.send(data);
+  //   });
+  // }).select('+password'); // obrigation set the password of BD
+
 });
 
 module.exports = router;
